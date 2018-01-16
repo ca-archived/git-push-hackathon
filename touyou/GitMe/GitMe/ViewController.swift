bd15e473564fab711125fca29da1c2ff81ef03a4
touyou <fujiyou1121@gmail.com>
2018-01-16 13:41:44 +0900

    setup projects

---
+++ touyou/GitMe/GitMe/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  GitMe
+//
+//  Created by 藤井陽介 on 2018/01/16.
+//  Copyright © 2018 touyou. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
