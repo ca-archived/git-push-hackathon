eda56d56661343f24d073c2fbd661ec91b6c131a
Tomoya Matsuyama <m.tomoya0221@gmail.com>
2018-01-25 18:16:50 +0900

    add Feeds Model

---
+++ tomoyamatsuyama/Hackathon/Hackathon/Models/Feeds.swift
@@ -0,0 +1,22 @@
+//
+//  Feeds.swift
+//  Hackathon
+//
+//  Created by Tomoya Matsuyama on 2018/01/25.
+//  Copyright © 2018年 Tomoya Matsuyama. All rights reserved.
+//
+
+import Foundation
+
+struct Feeds {
+    var avatarUrlString = ""
+    var name = ""
+    var title = ""
+    
+    mutating func initialize(){
+        self.avatarUrlString = ""
+            self.name = ""
+            self.title
+        
+    }
+}
