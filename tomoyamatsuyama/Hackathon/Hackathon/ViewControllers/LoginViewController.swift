51a13bf3cff07d2d002288a66cf5cb3fb96e9b44
Tomoya Matsuyama <m.tomoya0221@gmail.com>
2018-01-25 15:15:10 +0900

    add function

--- tomoyamatsuyama/Hackathon/Hackathon/ViewControllers/LoginViewController.swift
+++ tomoyamatsuyama/Hackathon/Hackathon/ViewControllers/LoginViewController.swift
@@ -10,7 +10,16 @@ import UIKit
 
 class LoginViewController: UIViewController {
 
+    @IBOutlet weak var webViewOfLogin: UIWebView!
+    
+    private func loadOfWebView(){
+        guard let loginUrl = URL(string: "https://github.com/login") else { return }
+        let request = URLRequest(url: loginUrl)
+        self.webViewOfLogin.loadRequest(request)
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        self.loadOfWebView()
     }
 }
