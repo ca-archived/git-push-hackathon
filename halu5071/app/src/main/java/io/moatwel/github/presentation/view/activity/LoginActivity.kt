9bc5a0d4a84e22cf169bcf4eec15d76ac55ba9e0
Yasunori Horii <horiiortho5@gmail.com>
2018-01-26 14:29:18 +0900

    #10 feat: Add basis of LoginActivity

--- halu5071/app/src/main/java/io/moatwel/github/presentation/view/activity/LoginActivity.kt
+++ halu5071/app/src/main/java/io/moatwel/github/presentation/view/activity/LoginActivity.kt
@@ -2,10 +2,23 @@ package io.moatwel.github.presentation.view.activity
 
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import dagger.android.AndroidInjection
+import io.moatwel.github.R
+import kotlinx.android.synthetic.main.activity_login.loginButton
 
 class LoginActivity : AppCompatActivity() {
 
   override fun onCreate(savedInstanceState: Bundle?) {
+    AndroidInjection.inject(this)
     super.onCreate(savedInstanceState)
+    setContentView(R.layout.activity_login)
+
+    loginButton.setOnClickListener {
+      openCustomTabs()
+    }
+  }
+
+  private fun openCustomTabs() {
+
   }
 }
