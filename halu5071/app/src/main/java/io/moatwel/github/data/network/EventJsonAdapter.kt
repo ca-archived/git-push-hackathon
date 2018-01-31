cf636b4c1edf2cb4462afd8e79bd8f4f0869cb72
Yasunori Horii <horiiortho5@gmail.com>
2018-01-31 23:50:35 +0900

    #42 test: Update Test

--- halu5071/app/src/main/java/io/moatwel/github/data/network/EventJsonAdapter.kt
+++ halu5071/app/src/main/java/io/moatwel/github/data/network/EventJsonAdapter.kt
@@ -1,11 +1,16 @@
 package io.moatwel.github.data.network
 
-import com.squareup.moshi.*
+import com.squareup.moshi.FromJson
+import com.squareup.moshi.ToJson
+import com.squareup.moshi.JsonAdapter
+import com.squareup.moshi.JsonReader
+import com.squareup.moshi.JsonWriter
+import com.squareup.moshi.Moshi
+import com.squareup.moshi.Types
 import io.moatwel.github.domain.entity.Actor
 import io.moatwel.github.domain.entity.event.*
 import java.lang.reflect.Type
-import java.util.*
-
+import java.util.Date
 
 class EventJsonAdapter(val moshi: Moshi) : JsonAdapter<List<Event?>>() {
 
@@ -16,21 +21,21 @@ class EventJsonAdapter(val moshi: Moshi) : JsonAdapter<List<Event?>>() {
     val list = arrayListOf<Event>()
     while (reader.hasNext()) {
       reader.beginObject()
-      reader.nextName()   // for id
+      reader.nextName() // for id
       val id = reader.nextLong()
-      reader.nextName()   // for type
+      reader.nextName() // for type
       val type = reader.nextString()
-      reader.nextName()   // for actor
+      reader.nextName() // for actor
       val actor = moshi.adapter(Actor::class.java).fromJson(reader)
-      reader.nextName()   // for repo
+      reader.nextName() // for repo
       val repo = moshi.adapter(EventRepository::class.java).fromJson(reader)
-      reader.nextName()   // for payload
+      reader.nextName() // for payload
       val payload = parsePayload(type, reader)
-      reader.nextName()   // for public
+      reader.nextName() // for public
       val isPublic = reader.nextBoolean()
-      reader.nextName()   // for created_at
+      reader.nextName() // for created_at
       val createdAt = moshi.adapter(Date::class.java).fromJson(reader)
-      reader.nextName()   // for org
+      reader.nextName() // for org
       val org = moshi.adapter(Actor::class.java).fromJson(reader)
 
       val event = Event(id, type, actor, repo, payload, createdAt, isPublic, org)
@@ -46,27 +51,31 @@ class EventJsonAdapter(val moshi: Moshi) : JsonAdapter<List<Event?>>() {
     TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
   }
 
-  private fun parsePayload(type: String, value: JsonReader): Payload? {
-    return when(type) {
-      "CommitCommentEvent" -> moshi.adapter(CommitCommentPayload::class.java).fromJson(value)
-      "CreateEvent" -> moshi.adapter(CreatePayload::class.java).fromJson(value)
-      "DeleteEvent" -> moshi.adapter(DeletePayload::class.java).fromJson(value)
-      "ForkEvent" -> moshi.adapter(ForkPayload::class.java).fromJson(value)
-      "GollumEvent" -> moshi.adapter(GollumPayload::class.java).fromJson(value)
-      "IssueCommentEvent" -> moshi.adapter(IssueCommentPayload::class.java).fromJson(value)
-      "PullRequestEvent" -> moshi.adapter(PullRequestPayload::class.java).fromJson(value)
+  private fun parsePayload(type: String, value: JsonReader): Payload {
+    return when (type) {
+      "CommitCommentEvent"
+        -> moshi.adapter(CommitCommentPayload::class.java).fromJson(value) as CommitCommentPayload
+      "CreateEvent" -> moshi.adapter(CreatePayload::class.java).fromJson(value) as CreatePayload
+      "DeleteEvent" -> moshi.adapter(DeletePayload::class.java).fromJson(value) as DeletePayload
+      "ForkEvent" -> moshi.adapter(ForkPayload::class.java).fromJson(value) as ForkPayload
+      "GollumEvent" -> moshi.adapter(GollumPayload::class.java).fromJson(value) as GollumPayload
+      "IssueCommentEvent"
+        -> moshi.adapter(IssueCommentPayload::class.java).fromJson(value) as IssueCommentPayload
+      "PullRequestEvent"
+        -> moshi.adapter(PullRequestPayload::class.java).fromJson(value) as PullRequestPayload
       "PullRequestReviewEvent"
-        -> moshi.adapter(PullRequestReviewPayload::class.java).fromJson(value)
+        -> moshi.adapter(PullRequestReviewPayload::class.java).fromJson(value) as PullRequestReviewPayload
       "PullRequestReviewCommentEvent"
-        -> moshi.adapter(PullRequestReviewCommentPayload::class.java).fromJson(value)
-      "PushEvent" -> moshi.adapter(PushPayload::class.java).fromJson(value)
-      "WatchEvent" -> moshi.adapter(WatchPayload::class.java).fromJson(value)
+        -> moshi.adapter(PullRequestReviewCommentPayload::class.java)
+                .fromJson(value) as PullRequestReviewCommentPayload
+      "PushEvent" -> moshi.adapter(PushPayload::class.java).fromJson(value) as PushPayload
+      "WatchEvent" -> moshi.adapter(WatchPayload::class.java).fromJson(value) as WatchPayload
       else -> throw Throwable("Unaccepted payload type: $type")
     }
   }
 
   companion object {
-    val FACTORY: Factory = object : Factory{
+    val FACTORY: Factory = object : Factory {
       override fun create(type: Type?,
                           annotations: MutableSet<out Annotation>?,
                           moshi: Moshi): EventJsonAdapter? {
--- halu5071/app/src/test/java/io/moatwel/github/data/network/EventJsonAdapterTest.kt
+++ halu5071/app/src/test/java/io/moatwel/github/data/network/EventJsonAdapterTest.kt
@@ -3,13 +3,17 @@ package io.moatwel.github.data.network
 import com.squareup.moshi.*
 import io.moatwel.github.TestUtil
 import io.moatwel.github.domain.entity.event.Event
+import io.moatwel.github.domain.entity.event.PullRequestReviewCommentPayload
+import io.moatwel.github.domain.entity.event.WatchPayload
+import org.hamcrest.CoreMatchers.`is`
 import org.junit.Assert.assertNotNull
+import org.junit.Assert.assertThat
 import org.junit.Before
 import org.junit.Test
 import org.junit.runner.RunWith
 import org.powermock.core.classloader.annotations.PrepareForTest
 import org.powermock.modules.junit4.PowerMockRunner
-import java.util.*
+import java.util.Date
 
 @RunWith(PowerMockRunner::class)
 @PrepareForTest(EventJsonAdapter::class)
@@ -35,6 +39,24 @@ class EventJsonAdapterTest {
     val list = adapter.fromJson(resource)
 
     assertNotNull(list)
-  }
+    assertNotNull(list?.get(0))
+    assertNotNull(list?.get(1))
+    assertNotNull(list?.get(2))
+    assertThat(list?.get(0)?.id, `is`(7163073730L))
+    assertThat(list?.get(0)?.type, `is`("WatchEvent"))
+    assertThat(list?.get(0)?.actor?.id, `is`(32922369L))
+    assertThat(list?.get(0)?.actor?.login, `is`("halubot"))
+    assertThat(list?.get(0)?.repo?.id, `is`(7508411L))
+    assertThat(list?.get(0)?.repo?.name, `is`("ReactiveX/RxJava"))
+    assertThat((list?.get(0)?.payload as WatchPayload).action, `is`("started"))
+    assertThat(list.get(1).id, `is`(7163070761L))
+    assertThat(list.get(1).type, `is`("PullRequestReviewCommentEvent"))
+    assertThat(list.get(1).actor?.id, `is`(1386930L))
+    assertThat(list.get(1).actor?.login, `is`("takahirom"))
+    assertThat(list.get(1).repo?.id, `is`(115203383L))
+    assertThat(list.get(1).repo?.name, `is`("DroidKaigi/conference-app-2018"))
+    assertThat((list.get(1).payload as PullRequestReviewCommentPayload).action,
+      `is`("created"))
 
+  }
 }
