07b698d4fdcbf5fca37f0c657f40982fef1f7457
Tomoya Matsuyama <m.tomoya0221@gmail.com>
2018-01-31 12:34:46 +0900

    add header view

--- tomoyamatsuyama/Hackathon/Hackathon/Views/FollowDataTableViewCell.swift
+++ tomoyamatsuyama/Hackathon/Hackathon/Views/FollowDataTableViewCell.swift
@@ -12,13 +12,10 @@ class FollowDataTableViewCell: UITableViewCell {
 
     override func awakeFromNib() {
         super.awakeFromNib()
-        // Initialization code
     }
 
     override func setSelected(_ selected: Bool, animated: Bool) {
         super.setSelected(selected, animated: animated)
-
-        // Configure the view for the selected state
     }
 
 }
