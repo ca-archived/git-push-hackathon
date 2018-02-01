3887852e5697732a4b599ae04eed5d8831c0b5b9
Yasunori Horii <horiiortho5@gmail.com>
2018-02-01 19:58:54 +0900

    44 feat: Add dagger dependency

--- halu5071/app/src/main/java/io/moatwel/github/data/network/AppJsonAdapterFactory.kt
+++ halu5071/app/src/main/java/io/moatwel/github/data/network/AppJsonAdapterFactory.kt
@@ -1,2 +1,11 @@
 package io.moatwel.github.data.network
 
+import com.squareup.moshi.JsonAdapter
+import se.ansman.kotshi.KotshiJsonAdapterFactory
+
+@KotshiJsonAdapterFactory
+abstract class AppJsonAdapterFactory : JsonAdapter.Factory {
+  companion object {
+    val INSTANCE : AppJsonAdapterFactory = KotshiAppJsonAdapterFactory()
+  }
+}
