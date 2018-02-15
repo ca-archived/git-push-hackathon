4250ef9dd33705ab23cc68f1a06ad83a836db427
churabou <ginga7888@gmail.com>
2018-02-16 08:54:53 +0900

    create project

---
+++ churabou/churabou/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  churabou
+//
+//  Created by ちゅーたつ on 2018/02/16.
+//  Copyright © 2018年 ちゅーたつ. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
