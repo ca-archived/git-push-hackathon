deee817a8a909a39c643f9e3c98accd208cb10fe
Yasunori Horii <horiiortho5@gmail.com>
2018-02-18 17:06:54 +0900

    feat: Add EventJsonAdapter to moshi

--- halu5071/app/src/main/java/io/moatwel/github/presentation/di/NetworkModule.kt
+++ halu5071/app/src/main/java/io/moatwel/github/presentation/di/NetworkModule.kt
@@ -30,6 +30,7 @@ import com.squareup.moshi.Rfc3339DateJsonAdapter
 import dagger.Module
 import dagger.Provides
 import io.moatwel.github.data.network.AppJsonAdapterFactory
+import io.moatwel.github.data.network.EventJsonAdapter
 import io.moatwel.github.data.network.HeaderInterceptor
 import io.moatwel.github.data.network.retrofit.EventApi
 import io.moatwel.github.data.network.retrofit.UserApi
@@ -86,6 +87,7 @@ class NetworkModule {
     return Moshi.Builder()
       .add(KotlinJsonAdapterFactory())
       .add(AppJsonAdapterFactory.INSTANCE)
+      .add(EventJsonAdapter.FACTORY)
       .add(Date::class.java, Rfc3339DateJsonAdapter())
       .build()
   }
