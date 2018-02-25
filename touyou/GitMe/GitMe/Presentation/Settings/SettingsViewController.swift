002c76b7974442ada7375f0443900eb3145dee18
touyou <fujiyou1121@gmail.com>
2018-02-25 13:25:37 +0900

    一旦iosは最終日までfix

---
+++ touyou/GitMe/GitMe/Presentation/Settings/SettingsViewController.swift
@@ -0,0 +1,29 @@
+//
+//  SettingsViewController.swift
+//  GitMe
+//
+//  Created by 藤井陽介 on 2018/02/25.
+//  Copyright © 2018 touyou. All rights reserved.
+//
+
+import UIKit
+
+
+// MARK: - SettingsViewController
+
+class SettingsViewController: UIViewController {
+
+
+    // MARK: Internal
+
+
+    // MARK: UIViewController
+
+    override func viewDidLoad() {
+
+        super.viewDidLoad()
+    }
+
+    // MARK: Private
+    @IBOutlet private weak var tableView: UITableView!
+}
