bb84c15b4594af5f1493b976424264f2dcb372ef
touyou <fujiyou1121@gmail.com>
2018-02-26 17:49:50 +0900

    feat(api): #4 WIP implement android api

--- touyou/GitMeAndroid/app/src/main/java/com/dev/touyou/gitme/Data/GitHubAPI.kt
+++ touyou/GitMeAndroid/app/src/main/java/com/dev/touyou/gitme/Data/GitHubAPI.kt
@@ -2,31 +2,75 @@ package com.dev.touyou.gitme.Data
 
 import android.content.SharedPreferences
 import io.reactivex.Observable
+import okhttp3.OkHttpClient
+import retrofit2.http.GET
+import retrofit2.http.Path
+import retrofit2.http.Query
 import java.net.URL
+import java.util.prefs.Preferences
+import kotlin.properties.Delegates
 
 /**
  * Created by touyou on 2018/02/25.
  */
 
+interface GitHubAPIInterface {
+
+    @GET("/user")
+    fun getCurrentUser(
+            @Query("access_token") accessToken: String): Observable<User>
+
+    @GET("/users/{userName}/received_events")
+    fun getEvents(
+            @Path("userName") userName: String,
+            @Query("access_token") accessToken: String,
+            @Query("page") page: Int,
+            @Query("per_page") perPage: Int): Observable<Array<Event>>
+}
+
 object GitHubAPI {
 
+    /// Check log in or not
     val isLoggedIn: Boolean
         get() {
 
-            return false
+
+            return cache.get(DefaultKeys.OAuthKey.rawValue, "") != ""
         }
 
     init {
-
-
     }
 
+    /// Log in function
     fun logIn(): Observable<User> {
 
-        return Observable.empty()
+        if (cache.get(DefaultKeys.OAuthKey.rawValue, "") != "") {
+
+            return fetchUser()
+        }
+
+        return authorize()
+    }
+
+    /// Log out function
+    fun logOut() {
+
+        cache.put(DefaultKeys.OAuthKey.rawValue, "")
+        cache.put(DefaultKeys.UserName.rawValue, "")
+        cache.put(DefaultKeys.UserIcon.rawValue, "")
     }
 
     fun fetchUser(): Observable<User> {
+
+        if (cache.get(DefaultKeys.UserName.rawValue) != "" && cache.get(DefaultKeys.UserIcon.rawValue) != "") {
+
+            val userName = cache.get(DefaultKeys.UserName.rawValue, "")
+            val userIconUrl = URL(cache.get(DefaultKeys.UserIcon.rawValue))
+            val user = User(userName, 0, userIconUrl)
+            return Observable.just(user)
+        }
+
+        // TODO: ここでGitHubAPIInterfaceのリクエストを叩く?
 
         return Observable.empty()
     }
@@ -43,7 +87,8 @@ object GitHubAPI {
 
     private val base: String = "https://api.github.com"
     private val oauthKey: String? = null
-    private val cache: SharedPreferences? = null
+    private val cache: Preferences = Preferences.userRoot().node("GitMe")
+    private val okHttpClient: OkHttpClient.Builder = OkHttpClient.Builder()
 
     private fun authorize(): Observable<User> {
 
--- touyou/GitMeAndroid/app/src/main/java/com/dev/touyou/gitme/Extensions/Date+Extensions.kt
+++ touyou/GitMeAndroid/app/src/main/java/com/dev/touyou/gitme/Extensions/Date+Extensions.kt
@@ -1,13 +1,31 @@
 package com.dev.touyou.gitme.Extensions
 
+import java.text.SimpleDateFormat
 import java.util.*
 
 /**
  * Created by touyou on 2018/02/26.
  */
 
-fun Date.yearsFrom(): String {
+val Date.yearsFrom: Int
+    get() {
+        val diff = this.compareTo(Date())
+        return 0
+    }
 
-    
-    return ""
-}
+val Date.offsetString: String
+    get() {
+
+        if (yearsFrom > 0) {
+
+            return "$yearsFrom years ago"
+        }
+
+        return "now"
+    }
+
+val Date.dateString: String
+    get() {
+
+        return SimpleDateFormat("yyyy/MM/dd", Locale.JAPAN).format(this)
+    }
