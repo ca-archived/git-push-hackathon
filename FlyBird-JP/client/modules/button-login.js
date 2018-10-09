f9129c32aa51e8df981de0ed84272a1d9330ed88
FlyBird-JP <fb@flybird.jp>
2018-10-10 06:13:21 +0900

    [add] クライアントサイドの認証処理

--- FlyBird-JP/client/modules/button-login.js
+++ FlyBird-JP/client/modules/button-login.js
@@ -5,7 +5,7 @@ const template = `
              color: #fff;
              height: 100%;
         }
-        #login {
+        #login,#logout {
             display: block;
             width: 200px;
             height: calc(100% - 1em);
@@ -17,20 +17,21 @@ const template = `
             font-size: large;
             cursor: pointer;
             font-weight: 100;
-            font-family:"游ゴシック", "YuGothic", "Yu Gothic", 'ヒラギノ角ゴ ProN W3', "Hiragino Kaku Gothic ProN", "Meiryo", Helvetica, Arial, sans-serif;
+            font-family:'游ゴシック', 'YuGothic', 'Yu Gothic', 'ヒラギノ角ゴ ProN W3', 'Hiragino Kaku Gothic ProN', 'Meiryo', Helvetica, Arial, sans-serif;
             outline: 0;
             transition: background ease-in-out 0.3s;
         }
-        #login:hover {
+        #login:hover,#logout:hover {
             background: #fff;
             color: #333;
             font-weight: 500;
         }
-        #login:disabled{
+        #login:disabled, #logout:disabled {
             display: none;
         }
     </style>
-    <button id="login" disabled="disabled">Login</button>
+    <button id='login' disabled='disabled'>Login</button>
+    <button id='logout' disabled='disabled'>Login</button>
 `;
 
 class ButtonLogin extends HTMLElement {
@@ -39,23 +40,28 @@ class ButtonLogin extends HTMLElement {
         this.attachShadow({ 'mode': 'open' })
         this.shadowRoot.innerHTML = template
         let accessToken = localStorage.getItem('accessToken')
-        const url = new URL(location.href)
         if (accessToken == null) {
-            if (url.searchParams.has("code")) {
-
-            }
-            else {
-                this.shadowRoot.getElementById("login").disabled = false
-                this.shadowRoot.getElementById("login").addEventListener("click", (event) => {
-                    location.href = "https://github.com/login/oauth/authorize?client_id=a51e816845d833008db2"
-                })
-            }
+            this.shadowRoot.getElementById('login').disabled = false
+            this.shadowRoot.getElementById('login').addEventListener('click', (event) => {
+                location.href = `https://github.com/login/oauth/authorize?client_id=${this.getAttribute("clientId")}`
+            })
+        } else {
+            this.shadowRoot.getElementById('logout').disabled = false
+            this.shadowRoot.getElementById('logout').addEventListener('click', (event) => {
+                localStorage.clear('accessToken')
+                location.href = new URL(location.href).origin
+            })
         }
     }
     static get observedAttributes() {
         return [];
     }
     connectedCallback() {
+    }
+    attributeChangedCallback(name, oldValue, newValue) {
+        if (oldValue != newValue) switch (name) {
+            default:break;
+        }
     }
 }
 
