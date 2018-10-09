d0a521fecf69a3ea85741b2d624d87c4f60fe9ab
Asck <asck.bbc@gmail.com>
2018-10-09 18:28:49 +0900

    > ng generate component dashboard CREATE src/app/dashboard/dashboard.component.html (28 bytes) CREATE src/app/dashboard/dashboard.component.spec.ts (649 bytes) CREATE src/app/dashboard/dashboard.component.ts (281 bytes) CREATE src/app/dashboard/dashboard.component.css (0 bytes) UPDATE src/app/app.module.ts (408 bytes)

--- github-app/github-app/src/app/app.module.ts
+++ github-app/github-app/src/app/app.module.ts
@@ -2,10 +2,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DashboardComponent
   ],
   imports: [
     BrowserModule
