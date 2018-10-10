9335e9fe833d12c496a29820f4a5a9d3808efe9f
Asck <asck.bbc@gmail.com>
2018-10-10 10:05:45 +0900

    edit dependencies of components

--- github-app/github-app/src/app/app-routing.module.ts
+++ github-app/github-app/src/app/app-routing.module.ts
@@ -1,10 +1,18 @@
 import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import { RouterModule, Routes } from '@angular/router';
+import { CommandPanelComponent } from './command-panel/command-panel.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { MenuComponent } from './menu/menu.component';
+import { MessageComponent } from './message/message.component';
+
+const routes: Routes = [
+    { path: '', component: DashboardComponent },
+    { path: 'command-panel', component: CommandPanelComponent }
+];
 
 @NgModule({
-  imports: [
-    CommonModule
-  ],
-  declarations: []
+  exports: [ RouterModule ],
+  imports: [ RouterModule.forRoot(routes) ]
 })
+
 export class AppRoutingModule { }
