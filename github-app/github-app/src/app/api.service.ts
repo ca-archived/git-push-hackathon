64e65f81f037222e0eb4a49955dd30e915770c99
Asck <asck.bbc@gmail.com>
2018-10-10 17:06:32 +0900

    create simply api get test

--- github-app/github-app/src/app/api.service.ts
+++ github-app/github-app/src/app/api.service.ts
@@ -1,9 +1,33 @@
-import { Injectable } from '@angular/core';
+import {Injectable} from '@angular/core';
+import {Http, Response} from "@angular/http";
+import {HttpClient} from '@angular/common/http';
 
 @Injectable({
-  providedIn: 'root'
+    providedIn: 'root'
 })
 export class ApiService {
+    //URL = "https://github.com/login/oauth/authorize";
+    URL = "http://httpbin.org/ip";
 
-  constructor() { }
+    constructor(private http: HttpClient) {
+    }
+
+    GetTest() {
+        //http.getをするためのオブジェクトを生成
+        var httpObj = this.http.get(this.URL);
+        //成功時・失敗時の動作を指定する。
+        httpObj.subscribe(this.getSuccess, this.getError);
+    }
+
+    //http.getが成功した時に走るメソッド
+    getSuccess(response) {
+        console.log(response);
+    }
+
+    //http.getが失敗した時に走るメソッド
+    getError(error) {
+        console.log(error);
+    }
+
+
 }
--- github-app/github-app/src/app/command-panel/command-panel.component.ts
+++ github-app/github-app/src/app/command-panel/command-panel.component.ts
@@ -1,15 +1,22 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit} from '@angular/core';
+import { ApiService } from '../api.service';
 
 @Component({
-  selector: 'app-command-panel',
-  templateUrl: './command-panel.component.html',
-  styleUrls: ['./command-panel.component.css']
+    selector: 'app-command-panel',
+    templateUrl: './command-panel.component.html',
+    styleUrls: ['./command-panel.component.css']
 })
 export class CommandPanelComponent implements OnInit {
+    //URL = "https://github.com/login/oauth/authorize";
+    URL = "http://httpbin.org/ip";
 
-  constructor() { }
+    constructor(
+        private apiService : ApiService
+    ) {    }
 
-  ngOnInit() {
-  }
+    ngOnInit() {
+        console.log("command-panel OnInit");
+        this.apiService.GetTest();
+    }
 
 }
