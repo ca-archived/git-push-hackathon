70032b527c41bf68eaa38e9aee4ebb85891d5f5e
Asck <asck.bbc@gmail.com>
2018-10-10 17:05:48 +0900

    edit indent

--- github-app/github-app/src/app/app.module.ts
+++ github-app/github-app/src/app/app.module.ts
@@ -1,26 +1,29 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
 
-import { AppComponent } from './app.component';
-import { DashboardComponent } from './dashboard/dashboard.component';
-import { MessageComponent } from './message/message.component';
-import { MenuComponent } from './menu/menu.component';
-import { CommandPanelComponent } from './command-panel/command-panel.component';
-import { AppRoutingModule } from './app-routing.module';
+import {AppComponent} from './app.component';
+import {DashboardComponent} from './dashboard/dashboard.component';
+import {MessageComponent} from './message/message.component';
+import {MenuComponent} from './menu/menu.component';
+import {CommandPanelComponent} from './command-panel/command-panel.component';
+import {AppRoutingModule} from './app-routing.module';
+import {HttpClientModule} from '@angular/common/http';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    DashboardComponent,
-    MessageComponent,
-    MenuComponent,
-    CommandPanelComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+    declarations: [
+        AppComponent,
+        DashboardComponent,
+        MessageComponent,
+        MenuComponent,
+        CommandPanelComponent
+    ],
+    imports: [
+        BrowserModule,
+        AppRoutingModule,
+        HttpClientModule
+    ],
+    providers: [],
+    bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
