52f5c48cf7a1c6e1eebd83fb2b42bab3fe38d7ca
Ryotaro Miyagwa <o_o_o_o_o1031@yahoo.co.jp>
2018-10-11 22:27:58 +0900

    SPA化

--- Dragon-taro/app.js
+++ Dragon-taro/app.js
@@ -1,10 +1,58 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import { HashRouter, Route, Link, Switch } from "react-router-dom";
 
 class App extends React.Component {
+  constructor() {
+    super();
+  }
+
   render() {
-    return <div>hello react!</div>;
+    return (
+      <HashRouter>
+        <div>
+          <ul>
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+
+            <li>
+              <Link to="/about">About</Link>
+            </li>
+
+            <li>
+              <Link to="/friends">Friends</Link>
+            </li>
+          </ul>
+          <hr />
+          <Switch>
+            <Route exact path="/" component={Home} />
+            <Route path="/about" component={About} />
+            <Route path="/friends" component={Friends} />
+          </Switch>
+        </div>
+      </HashRouter>
+    );
   }
 }
+
+const Home = () => (
+  <div>
+    <h2>Home</h2>
+    <p>Welcome to ようこそ</p>
+  </div>
+);
+const About = () => (
+  <div>
+    <h2>About</h2>
+    <p>フレンズに投票するページです</p>
+  </div>
+);
+const Friends = () => (
+  <div>
+    <h2>Friends</h2>
+    <p>ここにフレンズのリストを書きます</p>
+  </div>
+);
 
 ReactDOM.render(<App />, document.getElementById("root"));
