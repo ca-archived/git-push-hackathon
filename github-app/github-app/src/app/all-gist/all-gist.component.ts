17df2fda534c923b212150c58aaa0436772ab15b
Asck <asck.bbc@gmail.com>
2018-10-13 21:53:53 +0900

    get gist id ,gist owner name

--- github-app/github-app/src/app/all-gist/all-gist.component.ts
+++ github-app/github-app/src/app/all-gist/all-gist.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit ,  Renderer2 } from '@angular/core';
 import { ApiService } from '../api.service';
 
 @Component({
@@ -9,13 +9,28 @@ import { ApiService } from '../api.service';
 export class AllGistComponent implements OnInit {
 
   constructor(
-      private apiService : ApiService
+      private apiService : ApiService,
+      private renderer : Renderer2
   ) { }
 
   ngOnInit() {
     //this.apiService.GithubApiTest();
     this.apiService.GithubApiOAuthTest();
-    this.apiService.GithubApiOAuth_gist();
+    this.apiService.GithubApiOAuth_gist().then(this.appendGistScript);
+
+    //this.appendGistScript();
+
   }
+
+  appendGistScript() {
+    var gist_script = this.renderer.createElement("script");
+    console.log("debug point");
+    console.log("gist_owner_name");
+    console.log(this.apiService.gist_owner_name);
+    gist_script.setAttribute("src", "https://gist.github.com/"+this.apiService.gist_owner_name+"/"+this.apiService.gist_id+".js");
+    var div = document.getElementById('gist_area');
+    div.parentNode.insertBefore(gist_script, div.nextSibling);
+  }
+
 
 }
--- github-app/github-app/src/app/api.service.ts
+++ github-app/github-app/src/app/api.service.ts
@@ -8,9 +8,8 @@ import {TEMP_ACCESS_KEYS} from'./temp-access-keys';
 })
 export class ApiService {
 
-    constructor(
-        private http: HttpClient
-    ) {    }
+    constructor(private http: HttpClient) {
+    }
 
     OAuthURL1: string = "https://github.com/login/oauth/authorize"; //GET user redirected here
     OAuthURL2: string = "https://github.com/login/oauth/access_token"; //POST
@@ -20,12 +19,15 @@ export class ApiService {
     clientSecret: string = TEMP_ACCESS_KEYS.clientSecret;
     redirectUrl: string = TEMP_ACCESS_KEYS.redirectUrl;
     state: string = TEMP_ACCESS_KEYS.state; //random文字列ってこれでいいの？
-    scope : string = "gist";
+    scope: string = "gist";
 
     redirect_code: string = "";
     redirect_state: string = "";
 
     access_token: string = TEMP_ACCESS_KEYS.access_token;
+
+    gist_id: string = "";
+    gist_owner_name: string = "";
 
     httpOptions = {
         header: new HttpHeaders({
@@ -89,23 +91,44 @@ export class ApiService {
         );
         httpObj.subscribe(this.RequestNext, this.RequestError);
     }
+
     GithubApiOAuthTest() {
         console.log("GithubApiOAuthTest");
         var httpObj = this.http.get(
             "https://api.github.com/"
-            +"?access_token=" + TEMP_ACCESS_KEYS.access_token
+            + "?access_token=" + TEMP_ACCESS_KEYS.access_token
         );
         httpObj.subscribe(this.RequestNext, this.RequestError);
     }
+
     GithubApiOAuth_gist() {
         console.log("GithubApiOAuth_gist");
         var httpObj = this.http.get(
             "https://api.github.com/"
-            +"gists"
-            +"?access_token=" + TEMP_ACCESS_KEYS.access_token
+            + "gists"
+            + "?access_token=" + TEMP_ACCESS_KEYS.access_token
         );
-        httpObj.subscribe(this.RequestNext, this.RequestError);
+        httpObj.subscribe(this.GithubApiOAuth_gistNext, this.RequestError);
     }
+
+    private GithubApiOAuth_gistNext(res) {
+        console.log("GithubApiOAuth_gistNext");
+        console.log("res");
+        console.log(res);
+        let gist_id: string = res["0"]["owner"]["login"];
+        let gist_owner_name: string = res["0"]["id"];
+
+        this.gist_id = gist_id;
+        this.gist_owner_name = gist_owner_name;
+
+        console.log("gist_id");
+        console.log(gist_id);
+        console.log("gist_owner_name");
+        console.log(gist_owner_name);
+
+
+    }
+
 
 
     private RequestNext(res) {
