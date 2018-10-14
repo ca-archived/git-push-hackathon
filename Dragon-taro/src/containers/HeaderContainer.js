3779ebd5b63073781eb6f3083da7e9fe6ae3a850
Ryotaro Miyagwa <o_o_o_o_o1031@yahoo.co.jp>
2018-10-14 21:49:28 +0900

    connectのstate変更

--- Dragon-taro/src/containers/HeaderContainer.js
+++ Dragon-taro/src/containers/HeaderContainer.js
@@ -2,7 +2,11 @@ import { connect } from "react-redux";
 import Header from "../components/Header";
 import mapDispatchToProps from "./mapDispatchToProps";
 
+function mapStateToProps(state) {
+  return state;
+}
+
 export default connect(
-  state => state,
+  mapStateToProps,
   mapDispatchToProps
 )(Header);
--- Dragon-taro/src/containers/HomeContainer.js
+++ Dragon-taro/src/containers/HomeContainer.js
@@ -2,7 +2,11 @@ import { connect } from "react-redux";
 import Home from "../components/Home";
 import mapDispatchToProps from "./mapDispatchToProps";
 
+function mapStateToProps(state) {
+  return state;
+}
+
 export default connect(
-  state => state,
+  mapStateToProps,
   mapDispatchToProps
 )(Home);
