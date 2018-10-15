0f2ae83ea0a8a3ba8dd4f340aadf3edc8d320cf3
Asck <asck.bbc@gmail.com>
2018-10-15 23:28:01 +0900

    add post form html css

--- github-app/github-app/src/app/app.module.ts
+++ github-app/github-app/src/app/app.module.ts
@@ -1,8 +1,10 @@
 import {BrowserModule} from '@angular/platform-browser';
 import {NgModule} from '@angular/core';
-
 import {AppRoutingModule} from './app-routing.module';
+
 import { FormsModule }      from '@angular/forms';
+import { ReactiveFormsModule } from '@angular/forms';
+
 import {HttpClientModule} from '@angular/common/http';
 
 import {AppComponent} from './app.component';
@@ -43,6 +45,7 @@ import { GistModule } from '@sgbj/angular-gist';
         AppRoutingModule,
         HttpClientModule,
         FormsModule,
+        ReactiveFormsModule,
         GistModule
     ],
     entryComponents: [
--- github-app/github-app/src/app/post-gist/post-gist.component.ts
+++ github-app/github-app/src/app/post-gist/post-gist.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { ApiService } from '../api.service';
+import { FormControl , FormGroup} from '@angular/forms'
 
 @Component({
   selector: 'app-post-gist',
@@ -8,11 +9,23 @@ import { ApiService } from '../api.service';
 })
 export class PostGistComponent implements OnInit {
 
+  postGistForm = new FormGroup({
+    description : new FormControl(''),
+    release : new FormControl(''),
+    fileName : new FormControl(''),
+    content: new FormControl(''),
+  });
+
   constructor(
       private apiService : ApiService
   ) { }
 
   ngOnInit() {
+  }
+
+  onSubmit() {
+    this.apiService.GetPostGistReq();
+
   }
 
 }
