5fee41dc72ccb87c8f463a1fb693f407b39d1dd2
Asck <asck.bbc@gmail.com>
2018-10-15 20:00:20 +0900

    remove message component

--- github-app/github-app/src/app/app.module.ts
+++ github-app/github-app/src/app/app.module.ts
@@ -1,35 +1,42 @@
 import {BrowserModule} from '@angular/platform-browser';
 import {NgModule} from '@angular/core';
 
+import {AppRoutingModule} from './app-routing.module';
+import { FormsModule }      from '@angular/forms';
+import {HttpClientModule} from '@angular/common/http';
+
 import {AppComponent} from './app.component';
 import {DashboardComponent} from './dashboard/dashboard.component';
-import {MessageComponent} from './message/message.component';
 import {MenuComponent} from './menu/menu.component';
 import {CommandPanelComponent} from './command-panel/command-panel.component';
-import {AppRoutingModule} from './app-routing.module';
-import {HttpClientModule} from '@angular/common/http';
 import { AllGistComponent } from './all-gist/all-gist.component';
 import { PostGistComponent } from './post-gist/post-gist.component';
 import { OauthComponent } from './oauth/oauth.component';
 import { OauthRedirectComponent } from './oauth-redirect/oauth-redirect.component';
 
-import { FormsModule }      from '@angular/forms';
-import { GistModule } from '@sgbj/angular-gist';
 import { GistSingleComponent } from './gist-single/gist-single.component';
+import { GistHtmlComponent } from './gist-single/gist-html.component';
+import { GistDirective } from './gist-single/gist.directive';
+import { GistService } from './gist-single/gist.service';
+
+import { GistModule } from '@sgbj/angular-gist';
 
 
 @NgModule({
     declarations: [
         AppComponent,
         DashboardComponent,
-        MessageComponent,
         MenuComponent,
         CommandPanelComponent,
         AllGistComponent,
         PostGistComponent,
         OauthComponent,
         OauthRedirectComponent,
-        GistSingleComponent
+
+        GistSingleComponent,
+        GistHtmlComponent,
+        GistDirective,
+
     ],
     imports: [
         BrowserModule,
@@ -38,7 +45,10 @@ import { GistSingleComponent } from './gist-single/gist-single.component';
         FormsModule,
         GistModule
     ],
-    providers: [],
+    entryComponents: [
+        GistHtmlComponent,
+    ],
+    providers: [GistService],
     bootstrap: [AppComponent]
 })
 export class AppModule {
