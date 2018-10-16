53f81e0d01923176f9d6ec0886871c7d1e137175
tockn <s.taku.inthebox@gmail.com>
2018-10-16 15:06:59 +0900

    oauthサーバのエンドポイントを環境変数から取ってくるように

--- tockn/view/src/components/pages/Home.vue
+++ tockn/view/src/components/pages/Home.vue
@@ -4,9 +4,12 @@
 
 <script>
 
+const endpoint = process.env.OAUTH_ENDPOINT
+
 export default {
   methods: {
     login () {
+      location.href = `${endpoint}/auth/github?redirect_url=${location.href}/auth/callback`
     }
   }
 }
