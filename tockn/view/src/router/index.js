12dfe3e2c294a5129db0821fd40dd0e381bfbfcc
tockn <s.taku.inthebox@gmail.com>
2018-10-16 14:14:46 +0900

    rm callback from router

--- tockn/view/src/router/index.js
+++ tockn/view/src/router/index.js
@@ -1,7 +1,6 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import Home from '@/components/pages/Home'
-import Callback from '@/components/pages/Callback'
 
 Vue.use(Router)
 
@@ -12,11 +11,6 @@ export default new Router({
       path: '/',
       name: 'Home',
       component: Home
-    },
-    {
-      path: '/auth/callback',
-      name: 'Callback',
-      component: Callback
     }
   ]
 })
