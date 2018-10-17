9ae77ac23764231f601400b1ce45ea39a9dfefca
tockn <s.taku.inthebox@gmail.com>
2018-10-18 02:50:58 +0900

    add gistcard

---
+++ tockn/view/src/components/modules/GistCard.vue
@@ -0,0 +1,25 @@
+<template>
+ <div class="card">
+   <div class="title">
+     {{ title }}
+   </div>
+   <div class="content">
+     {{ content }}
+   </div>
+ </div>
+</template>
+
+<script>
+
+export default {
+  props: {
+    title: String,
+    content: String
+  }
+}
+
+</script>
+
+<style>
+
+</style>
