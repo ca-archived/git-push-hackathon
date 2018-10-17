7c6fac87c7bf42eb5107ec6c40aa97621ec930eb
Ryotaro Miyagwa <o_o_o_o_o1031@yahoo.co.jp>
2018-10-18 02:24:21 +0900

    微修正

--- Dragon-taro/src/reducers/reducers.js
+++ Dragon-taro/src/reducers/reducers.js
@@ -2,4 +2,4 @@ import { combineReducers } from "redux";
 import { oauth, user } from "./oauth";
 import { gists, gist, editor } from "./gists";
 
-export default combineReducers({ oauth, user, gists, gist });
+export default combineReducers({ oauth, user, gists, gist, editor });
