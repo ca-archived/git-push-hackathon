fa36a02acf82586fa41fcc281de99688c19cbc9a
tockn <s.taku.inthebox@gmail.com>
2018-10-17 23:44:04 +0900

    create gist page

---
+++ tockn/view/src/components/pages/Gist.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+    <div v-if="gist" v-for="(file, index) in gist.files" :key="index">
+      <pre>{{ file.content }}</pre>
+    </div>
+  </div>
+</template>
+
+<script>
+import store from '../../store/index'
+
+export default {
+  computed: {
+    gist () {
+      return store.state.gist || false
+    }
+  },
+  created () {
+    store.dispatch('getGist', this.$route.params.id)
+  }
+}
+
+</script>
