635f800cd3fde74abd6b848c06f1674bed08fe90
Asck <asck.bbc@gmail.com>
2018-10-19 17:04:03 +0900

    edit comments organize some codes into functions

--- oauth-server/oauth-server/oauth-server/Program.cs
+++ oauth-server/oauth-server/oauth-server/Program.cs
@@ -17,22 +17,68 @@ namespace oauth_server
 
         public static class GlobalVals
         {
+            /*
+             * Program.csクラス内で使用する変数を定義
+             */
 
             public const string SERVER_ORIGIN = "http://localhost:4201/";
             public const string CLIENT_ORIGIN = "http://localhost:4200/";
 
-            public const string STEP2_REDIRECT_URL = SERVER_ORIGIN + "/step2-redirect";
-            public const string CLIENT_ID = "0bc6d4e0794201162940";
             public const string CLIENT_SECRET = "473798ee1cdead1152b7a66e164e7bb1b873f049";
             public const string OAUTH_URL2 = "https://github.com/login/oauth/access_token";
-            public static string code = "";
-            public const string STATE = "";
+        }
 
-            public static string access_token = "";
+        static string parseUrlParamsToJsonText(string urlParamsText)
+        {
+            /*
+             * url クエリ文字列を、Json形式の文字列に変換する
+             * 例　
+             * "param1=AAA&param2=BBB" 
+             * --> "{{ "param1" : "AAA" },{ "param2" : "BBB"}}"
+             */
+            string jsonText = "";
+            Dictionary<string, string> jsonDict = new Dictionary<string, string>();
+
+            string[] paramTexts = urlParamsText.Split('&');
+            foreach (string paramText in paramTexts)
+            {
+                string[] paramPair = paramText.Split('=');
+
+                if (paramPair[1] == null) paramPair[1] = "";
+                jsonDict.Add(paramPair[0], paramPair[1]);
+            }
+
+            jsonText = JsonConvert.SerializeObject(jsonDict);
+            return jsonText;
+        }
+
+        static void responseToClient(HttpListenerResponse res, string resBody)
+        {
+            /*
+             * アクセスしたクライアントへのresponse処理部分
+             */
+            try
+            {
+                res.StatusCode = 200;
+                byte[] content = Encoding.UTF8.GetBytes(resBody);
+                res.OutputStream.Write(content, 0, content.Length);
+            }
+            catch (Exception ex)
+            {
+                res.StatusCode = 500; // 404 でも良いのだがここは雑に 500 にまとめておく
+                byte[] content = Encoding.UTF8.GetBytes(ex.Message);
+                res.OutputStream.Write(content, 0, content.Length);
+            }
+            res.Close();
         }
 
         static string apiPost(string url, Dictionary<string, string> reqParams)
         {
+            /*
+             * apiを投げる処理
+             * url       : apiを投げる先
+             * reqParams : POSTリクエストの際に送信するパラメータ(Dictionary)
+             */
             string bodyText = "";
 
             if (reqParams.Count != 0)
@@ -44,8 +90,8 @@ namespace oauth_server
                 }
                 bodyText.Remove(bodyText.Length - 1); //最後の文字を削除=余分な"&"
             }
-            //リクエスト本体 text -> ascii codes(json形式)
-            byte[] bodyAscii = Encoding.ASCII.GetBytes(bodyText);
+            //リクエスト本体 text -> ascii codes
+            byte[] bodyCodes = Encoding.ASCII.GetBytes(bodyText);
 
             ServicePointManager.SecurityProtocol
                 = SecurityProtocolType.Tls
@@ -58,18 +104,18 @@ namespace oauth_server
             request.Method = "POST";
             request.ContentType = "application/x-www-form-urlencoded";
             request.Accept = "application/x-www-form-urlencoded";
-            request.ContentLength = bodyAscii.Length;
+            request.ContentLength = bodyCodes.Length;
 
 
             //(HttpWebRequest).GetRequestStream : 要求データを書きこむために使用するStreamオブジェクトを取得する
             using (Stream reqStream = request.GetRequestStream())
             {
-                reqStream.Write(bodyAscii, 0, bodyAscii.Length);
+                reqStream.Write(bodyCodes, 0, bodyCodes.Length);
             }
-
+            //api.github.comサーバーに要求し、応答を取得 --> return
             try
             {
-                WebResponse response = request.GetResponse(); //api.github.comサーバーに要求
+                WebResponse response = request.GetResponse();
                 string resText;
                 using (Stream responseStream = response.GetResponseStream())
                 {
@@ -89,82 +135,40 @@ namespace oauth_server
 
         static void originAccess(HttpListenerRequest req, HttpListenerResponse res)
         {
-            string contentStr = "accesse in domain url";
-            res.ContentType = "text/html";
-            // ファイル内容を出力
-            try
-            {
-                res.StatusCode = 200;
-                byte[] content = File.ReadAllBytes(contentStr);
-                res.OutputStream.Write(content, 0, content.Length);
-            }
-            catch (Exception ex)
-            {
-                res.StatusCode = 500; // 404 でも良いのだがここは雑に 500 にまとめておく
-                byte[] content = Encoding.UTF8.GetBytes(ex.Message);
-                res.OutputStream.Write(content, 0, content.Length);
-            }
-            res.Close();
+            /*
+             * ディレクトリなしでアクセス
+             */
+            string contentStr = "{message :\"This server is for OAuth API test\"}";
+            res.ContentType = "application/json";
+            responseToClient(res, contentStr);
         }
 
         static void notFoundAccess(HttpListenerRequest req, HttpListenerResponse res)
         {
-            string contentStr = "404 Not Found Page";
-            res.ContentType = "text/plain";
-            // ファイル内容を出力
-            try
-            {
-                res.StatusCode = 200;
-                byte[] content = File.ReadAllBytes(contentStr);
-                res.OutputStream.Write(content, 0, content.Length);
-            }
-            catch (Exception ex)
-            {
-                res.StatusCode = 500; // 404 でも良いのだがここは雑に 500 にまとめておく
-                byte[] content = Encoding.UTF8.GetBytes(ex.Message);
-                res.OutputStream.Write(content, 0, content.Length);
-            }
-            res.Close();
+            /*
+             * クライアントがディレクトリのないところを参照
+             */
+            string contentStr = "{error_message :\"404 not Found\"}";
+            res.ContentType = "application/json";
+            responseToClient(res, contentStr);
         }
 
         static void OAuth2Post(HttpListenerRequest req, HttpListenerResponse res, Dictionary<string, string> reqParams)
         {
             /*
              * api.github.comサーバーにOAuthのaccess_tokenをcodeと引き換えに要求
-            */
-            string OAuthResText = "";
+             */
             reqParams.Add("client_secret", GlobalVals.CLIENT_SECRET);
-            OAuthResText = apiPost(GlobalVals.OAUTH_URL2, reqParams);//api.github.comにPOST
 
-            Dictionary<string, string> resDict = new Dictionary<string, string>();
+            string OAuthResText
+                = apiPost(GlobalVals.OAUTH_URL2, reqParams);//api.github.comにPOST
 
-            string[] paramtPairTexts = OAuthResText.Split('&');
+            string resJsonText = parseUrlParamsToJsonText(OAuthResText);
 
-            foreach (string paramtPairText in paramtPairTexts) {
-
-                string[] paramPair = paramtPairText.Split('=');
-                resDict.Add(paramPair[0], paramPair[1]);
-
-            }
-            string bodyText = JsonConvert.SerializeObject(resDict);
-            
             res.ContentType = "application/json";
             res.AddHeader("Access-Control-Allow-Origin", "*");
 
-            // ファイル内容を出力
-            try
-            {
-                res.StatusCode = 200;
-                byte[] content = Encoding.UTF8.GetBytes(bodyText);
-                res.OutputStream.Write(content, 0, content.Length);
-            }
-            catch (Exception ex)
-            {
-                res.StatusCode = 500; // 404 でも良いのだがここは雑に 500 にまとめておく
-                byte[] content = Encoding.UTF8.GetBytes(ex.Message);
-                res.OutputStream.Write(content, 0, content.Length);
-            }
-            res.Close();
+            responseToClient(res, resJsonText);
         }
 
         static void OAuthServer()
@@ -173,58 +177,42 @@ namespace oauth_server
             listener.Prefixes.Add(GlobalVals.SERVER_ORIGIN); // "http://localhost:xxxx/"
             listener.Start();
 
-            //req.RawUrl --> /directry-01A?param1=asdf&param2=
+            //req.RawUrl --> /directry-01_A/bb?param1=asdf&param2=&redirect_uri=//localhost:4201
 
-            //match "directry-01A"
+            //match "directry-01_A/bb"
             Regex dirReg =
-                new Regex("(?<=/)([a-z]|[A-Z]|[0-9]|-)+");
+                new Regex(@"(?<=\/)([a-z]|[A-Z]|[0-9]|-|_|/)+(?=\?)");
 
-            //match "param1=asdf" and "param2=" and "redirect_uri=//localhost:4201"
-            Regex paramsReg =
-                new Regex("(?<=[?|&])([a-z]|[A-Z]|[0-9]|-|_)+=([a-z]|[A-Z]|[0-9]|-|_|/|:)*");
-                //new Regex("(?<=[?|&])([a-z]|[A-Z]|[0-9]|-)+=([a-z]|[A-Z]|[0-9]|-)*");
+            //match "param1=asdf&param2=&redirect_uri=//localhost:4201"
+            Regex paramsReg=
+                new Regex(@"(?<=\?).*$");
 
             while (true)
             {
-                //アクセスがあるまでlistener.GetContext() でとまる
                 Console.WriteLine("Access waiting : ");
+
+                //アクセスがあるまでlistener.GetContext() でとまる
                 HttpListenerContext context = listener.GetContext();
                 HttpListenerRequest req = context.Request;
                 HttpListenerResponse res = context.Response;
-                //res.ContentType イメージ: image/拡張子 html: text/html　にする
-
-                // URL (ここには "/" とか "/index.html" 等が入ってくる)
-                string dir_params = req.RawUrl;
-
-                MatchCollection dirMatches = dirReg.Matches(req.RawUrl);
-                MatchCollection paramMatches = paramsReg.Matches(req.RawUrl);
-
-                string[] reqDirs =
-                    new string[0];
-
-                Dictionary<string, string> reqParams = new Dictionary<string, string>();
-
-                // MatchesObject --> [ "" , "" , ..]
-                foreach (Match dirMatch in dirMatches)
-                {
-                    int len = reqDirs.Length;
-                    Array.Resize(ref reqDirs, len + 1);
-                    reqDirs[len] = dirMatch.Value;
-                }
-
-                // MatchesObject --> { {"" : ""} , { "" : "" }, ..}
-                foreach (Match paramMatch in paramMatches)
-                {
-                    string[] param_pair
-                        = paramMatch.Value.Split('=');
-
-                    //paramの値がないときの処理
-                    if (param_pair[1] == null) param_pair[1] = "";
-
-                    reqParams.Add(param_pair[0], param_pair[1]);
-                }
 
                 Console.WriteLine("Access   in: " + req.RawUrl);
+
+                //requested directory --> ["directory1", "directory2", ...]
+                Match dirMatch = dirReg.Match(req.RawUrl);
+                string[] reqDirs = dirMatch.ToString().Split('/');
+
+                //requested parameters --> { {"" : ""} , { "" : "" }, ..}
+                Match paramMatch = paramsReg.Match(req.RawUrl);
+                Dictionary<string, string> reqParams = new Dictionary<string, string>();
+                string[] paramTexts = paramMatch.ToString().Split('&');
+                foreach (string paramText in paramTexts)
+                {
+                    string[] paramPair = paramText.Split('=');
+                    if (paramPair[1] == null) paramPair[1] = "";
+                    reqParams.Add(paramPair[0], paramPair[1]);
+                }
+
                 Console.WriteLine("directry in: ");
                 foreach (string dir in reqDirs)
                 {
@@ -237,7 +225,7 @@ namespace oauth_server
                 }
 
 
-
+                //アクセスディレクトリで処理を分岐
                 if (reqDirs[0] == null)
                 {
                     originAccess(req, res);
