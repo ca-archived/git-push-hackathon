b22b9b2e28578053c1684a9b571a21eb28bcaf63
tockn <s.taku.inthebox@gmail.com>
2018-10-20 10:44:11 +0900

    set background color

--- tockn/view/src/App.vue
+++ tockn/view/src/App.vue
@@ -11,6 +11,10 @@ export default {
 </script>
 
 <style>
+body {
+  background-color: #f5f5f5;
+}
+
 #app {
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
