3cda7970e2ff882ce65e45c80b3651a90ec3a0f9
tomoyamatsuyama <m.tomoya0221@gmail.com>
2018-10-20 12:33:56 +0900

    Update pod

--- tomoyamatsuyama/vol2/HackathonApp/HackathonApp/ViewController.swift
+++ tomoyamatsuyama/vol2/HackathonApp/HackathonApp/ViewController.swift
@@ -5,7 +5,6 @@
 //  Created by Tomoya Matsuyama on 2018/10/18.
 //  Copyright © 2018年 Tomoya Matsuyama. All rights reserved.
 //
-
 import UIKit
 
 class ViewController: UIViewController {
--- tomoyamatsuyama/vol2/HackathonApp/Pods/Target Support Files/Pods-HackathonApp/Pods-HackathonApp-frameworks.sh
+++ tomoyamatsuyama/vol2/HackathonApp/Pods/Target Support Files/Pods-HackathonApp/Pods-HackathonApp-frameworks.sh
@@ -144,11 +144,13 @@ strip_invalid_archs() {
 
 if [[ "$CONFIGURATION" == "Debug" ]]; then
   install_framework "${BUILT_PRODUCTS_DIR}/Alamofire/Alamofire.framework"
+  install_framework "${BUILT_PRODUCTS_DIR}/ReSwift/ReSwift.framework"
   install_framework "${BUILT_PRODUCTS_DIR}/RxCocoa/RxCocoa.framework"
   install_framework "${BUILT_PRODUCTS_DIR}/RxSwift/RxSwift.framework"
 fi
 if [[ "$CONFIGURATION" == "Release" ]]; then
   install_framework "${BUILT_PRODUCTS_DIR}/Alamofire/Alamofire.framework"
+  install_framework "${BUILT_PRODUCTS_DIR}/ReSwift/ReSwift.framework"
   install_framework "${BUILT_PRODUCTS_DIR}/RxCocoa/RxCocoa.framework"
   install_framework "${BUILT_PRODUCTS_DIR}/RxSwift/RxSwift.framework"
 fi
