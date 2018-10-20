db86f3e3b6bdad530196c8d4e9c89e62daab2008
tockn <s.taku.inthebox@gmail.com>
2018-10-20 10:43:43 +0900

    create loading

---
+++ tockn/view/src/components/Loading.vue
@@ -0,0 +1,22 @@
+<template>
+  <div id="loading"></div>
+</template>
+
+<style scoped>
+#loading {
+  position: absolute;
+  width: 32px;
+  height: 32px;
+  border-radius: 50%;
+  border: 8px solid black;
+  border-right-color: transparent;
+  animation: spin 1s linear infinite;
+}
+
+@keyframes spin {
+  0% {transform: rotate(0deg);}
+  50% {transform: rotate(180deg);}
+  100% {transform: rotate(360deg);}
+}
+
+</style>
