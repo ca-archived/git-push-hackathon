a2c96c217bd85cb1c1ae2cd40ce1d303172fd345
Asck <asck.bbc@gmail.com>
2018-10-20 13:21:30 +0900

    rename and move directories

---
+++ Kusunoki-19/github-app/src/app/all-gist-user/all-gist-user.component.ts
@@ -0,0 +1,34 @@
+import {Component, OnInit} from '@angular/core';
+import {ApiService} from '../api.service';
+import {GistService} from '../gist-single/gist.service';
+
+import {GistItem} from'../gist-single/gist-item';
+
+@Component({
+  selector: 'app-all-user-gist',
+  templateUrl: './all-gist-user.component.html',
+  styleUrls: ['./all-gist-user.component.css']
+})
+export class AllGistUserComponent implements OnInit {
+
+  gists : GistItem[];
+  readLimit : number = 10;
+  userName : string = "Kusunoki-19";
+
+  constructor(
+      private apiService: ApiService,
+      private gistService : GistService,
+  ) {}
+
+  ngOnInit() {
+    this.gistService.getGistItems_byUser(this.readLimit,this.userName);
+    this.gists = this.gistService.getGists(); //connect gistService.gistItem -- this.gists
+  }
+
+  reloadGists() {
+    this.gistService.getGistItems_byUser(this.readLimit,this.userName);
+    this.gists = this.gistService.getGists(); //connect gistService.gistItem -- this.gists
+  }
+
+
+}
