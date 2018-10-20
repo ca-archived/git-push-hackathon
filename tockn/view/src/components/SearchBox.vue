b3d518e4e87858a45281343012ed9c679f16e0e6
tockn <s.taku.inthebox@gmail.com>
2018-10-20 20:10:39 +0900

    create Searchbox and searchresultbox

---
+++ tockn/view/src/components/SearchBox.vue
@@ -0,0 +1,29 @@
+<template>
+  <input v-model="text" @input="changeText" class="search-input" placeholder="ユーザーを検索">
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      text: '',
+      timer: 0
+    }
+  },
+  methods: {
+    changeText () {
+      clearTimeout(this.timer)
+      this.timer = setTimeout(this.search.bind(this), 600)
+    },
+    search () {
+      this.$emit('search', this.text)
+    }
+  }
+}
+</script>
+
+<style scoped>
+.search-input {
+  width: 80%;
+}
+</style>
---
+++ tockn/view/src/components/SearchResultBox.vue
@@ -0,0 +1,60 @@
+<template>
+  <div class="card">
+    <loading v-show="searching" />
+    <div v-show="!searching">
+      <div v-show="result === undefined">
+        not exist.
+      </div>
+      <div v-show="result !== undefined">
+        <div class="avatar">
+          <img class="avatar" v-bind:src="avatarURL" alt="">
+        </div>
+        <div class="username">
+          {{ username }}
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import Loading from './Loading'
+
+export default {
+  props: {
+    result: Object,
+    searching: Boolean
+  },
+  computed: {
+    username () {
+      if (this.result === undefined) return
+      return this.result.login
+    },
+    avatarURL () {
+      if (this.result === undefined) return
+      return this.result.avatar_url
+    }
+  },
+  components: {
+    'loading': Loading
+  }
+}
+
+</script>
+
+<style scoped>
+
+.avatar {
+  width: 24px;
+  height: 24px;
+  display: inline-block;
+  vertical-align: middle;
+}
+
+.username {
+  margin: 0 4px 0 4px;
+  display: inline-block;
+  vertical-align: middle;
+}
+
+</style>
