aa856dad5202ad5f34f5ebcac757157ed4f00ec8
tockn <s.taku.inthebox@gmail.com>
2018-10-20 13:14:29 +0900

    prism.js使ってハイライトぉ

--- tockn/view/src/pages/Gist.vue
+++ tockn/view/src/pages/Gist.vue
@@ -1,12 +1,15 @@
 <template>
   <div>
     <div v-if="gist" v-for="(file, index) in gist.files" :key="index">
-      <pre>{{ file.content }}</pre>
+      <prism language="go">{{ file.content }}</prism>
     </div>
   </div>
 </template>
 
 <script>
+import Prism from 'vue-prism-component'
+import '../../static/prism'
+import '../../static/prism.css'
 import {mapGetters} from 'vuex'
 
 export default {
@@ -17,6 +20,11 @@ export default {
   },
   created () {
     this.$store.dispatch('gists/getGist', this.$route.params.id)
+  },
+  methods: {
+  },
+  components: {
+    'prism': Prism
   }
 }
 
