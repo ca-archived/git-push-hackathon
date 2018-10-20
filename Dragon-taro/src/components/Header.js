3700e997e25dd66d82c907b8acbb829c070da218
Ryotaro Miyagwa <o_o_o_o_o1031@yahoo.co.jp>
2018-10-20 17:05:59 +0900

    ログアウトのUI

--- Dragon-taro/src/components/Header.js
+++ Dragon-taro/src/components/Header.js
@@ -1,4 +1,4 @@
-import React, { Component } from "react";
+import React from "react";
 import { Link } from "react-router-dom";
 import { If } from "./parts/If";
 
@@ -15,12 +15,18 @@ const Header = ({ oauth, user, actions: { requestOAuth, logout } }) => {
           </button>
         </If>
         <If condition={oauth.isAuthorized && user.avatar_url}>
-          <div>
-            <button className="p-button">
-              <Link to="/gists/new">New Gist</Link>
-            </button>
-            <img src={user.avatar_url} alt="ユーザー画像" />
-            <button onClick={() => logout()}>Logout</button>
+          <div className="right-content">
+            <div className="button-wrapper">
+              <button className="p-button">
+                <Link to="/gists/new">New Gist</Link>
+              </button>
+            </div>
+            <div className="menu">
+              <img src={user.avatar_url} alt="ユーザー画像" />
+              <ul>
+                <li onClick={() => logout()}>Logout</li>
+              </ul>
+            </div>
           </div>
         </If>
       </div>
