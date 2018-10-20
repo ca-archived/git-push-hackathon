4dbe2e55b95d3b5eaab3a9a9d2ff87d7cca12e5d
tockn <s.taku.inthebox@gmail.com>
2018-10-20 23:20:14 +0900

    change bgcolor

--- tockn/view/src/globals/Header.vue
+++ tockn/view/src/globals/Header.vue
@@ -87,7 +87,7 @@ a {
 
 .header {
   overflow: hidden;
-  background-color: #20b2aa;
+  background-color: #a52a2a;
   box-shadow: 0px 1px 3px 0px rgba(0, 0, 0, 0.2);
   padding: 5px 10px;
   margin-bottom: 15px;
