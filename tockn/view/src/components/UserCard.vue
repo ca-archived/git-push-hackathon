7702253621cef5bad51a3a53b2a4282378d82cae
tockn <s.taku.inthebox@gmail.com>
2018-10-20 10:43:32 +0900

    create user card

---
+++ tockn/view/src/components/UserCard.vue
@@ -0,0 +1,38 @@
+<template>
+  <div class="card">
+    <div class="avatar">
+      <img class="avatar" v-bind:src="avatarURL" />
+    </div>
+    <div class="username">
+    <h2>{{ username }}'s Gists</h2>
+    </div>
+  </div>
+</template>
+
+<script>
+
+export default {
+  props: {
+    username: String,
+    avatarURL: String
+  }
+}
+
+</script>
+
+<style scoped>
+
+.avatar {
+  width: 72px;
+  height: 72px;
+  display: inline-block;
+  vertical-align: middle;
+}
+
+.username {
+  margin: 0 16px 0 16px;
+  display: inline-block;
+  vertical-align: middle;
+}
+
+</style>
