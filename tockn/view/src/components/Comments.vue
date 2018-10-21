5416b1d75d67f04168cc2ca0f1d2cba807a15a72
tockn <s.taku.inthebox@gmail.com>
2018-10-21 11:15:07 +0900

    create comments component

---
+++ tockn/view/src/components/Comments.vue
@@ -0,0 +1,33 @@
+<template>
+  <div>
+    <img class="comments-icon" src="../assets/comments.png" alt="">
+    <p class="comments-num">{{ num }}</p>
+  </div>
+</template>
+
+<script>
+
+export default {
+  props: {
+    num: Number
+  }
+}
+
+</script>
+
+<style scoped>
+
+.comments {
+  float: right;
+}
+
+.comments-icon {
+  width: 16px;
+  height: 16px;
+}
+
+.comments-num {
+  display: inline-block;
+}
+
+</style>
