476412e18c84eedd3af520354f493f52998ecbcf
tockn <s.taku.inthebox@gmail.com>
2018-10-21 21:55:26 +0900

    add buttons

--- tockn/view/src/pages/CreateGist.vue
+++ tockn/view/src/pages/CreateGist.vue
@@ -8,6 +8,11 @@
       :files="files"
       @setFilename="setFilename"
       @setContent="setContent" />
+    <div class="card">
+      <button class="add-btn">Add file</button>
+      <button class="public-btn">Create public gist</button>
+      <button class="secret-btn">Create secret gist</button>
+    </div>
   </div>
 </template>
 
@@ -37,6 +42,26 @@ export default {
 </script>
 
 <style scoped>
+
+button {
+  font-size: 20px;
+  height: 40px;
+  border-radius: 8px;
+}
+
+.add-btn {
+  float: left;
+}
+
+.secret-btn {
+  float: right;
+  background-color: #fffacd;
+  margin: 0 16px 0 16px;
+}
+
+.public-btn {
+  float: right;
+}
 
 .wrap {
   text-align: center;
