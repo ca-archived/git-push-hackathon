465e7cb71fae2e82770ad066d3c1bfe5853edeaa
tockn <s.taku.inthebox@gmail.com>
2018-10-21 19:08:47 +0900

    Editorとcreateをいい感じにバインディング

---
+++ tockn/view/src/components/Editor.vue
@@ -0,0 +1,78 @@
+<template>
+  <div class="card nopad">
+    <div class="filename">
+      <input class="input" v-model="filename" placeholder="Filename including extension...">
+    </div>
+    <codemirror v-model="content" :options="cmOptions"></codemirror>
+  </div>
+</template>
+
+<script>
+import {codemirror} from 'vue-codemirror'
+import 'codemirror/lib/codemirror.css'
+
+export default {
+  data () {
+    return {
+      cmOptions: {
+        tabSize: 4,
+        theme: 'base16-dark',
+        lineNumbers: true,
+        line: true
+      }
+    }
+  },
+  computed: {
+    filename: {
+      get () {
+        return this.files[this.index].filename
+      },
+      set (value) {
+        this.$emit('setFilename', this.index, value)
+      }
+    },
+    content: {
+      get () {
+        return this.files[this.index].content
+      },
+      set (value) {
+        this.$emit('setContent', this.index, value)
+      }
+    }
+  },
+  props: {
+    files: Array,
+    index: Number
+  },
+  methods: {
+    setFilename () {
+      this.$emit('setFilename', this.index, this.filename)
+    },
+    setContent () {
+      this.$emit('setContent', this.index, this.content)
+    }
+  },
+  components: {
+    codemirror
+  }
+}
+
+</script>
+
+<style scoped>
+
+.nopad {
+  text-align: left;
+  padding: 0 0 0 0;
+}
+
+.filename {
+  background-color: #efefefef;
+  margin: auto;
+}
+
+.filename .input {
+  width: 40%;
+}
+
+</style>
---
+++ tockn/view/src/pages/CreateGist.vue
@@ -0,0 +1,64 @@
+<template>
+  <div class="wrap">
+    <input class="description" v-model="description" placeholder="Gist description...">
+    <editor
+      v-for="(file, index) in files"
+      :key="index"
+      :index="index"
+      :files="files"
+      @setFilename="setFilename"
+      @setContent="setContent" />
+  </div>
+</template>
+
+<script>
+import Editor from '../components/Editor'
+
+export default {
+  data () {
+    return {
+      description: '',
+      files: [{}]
+    }
+  },
+  methods: {
+    setFilename (index, filename) {
+      this.files[index].filename = filename
+    },
+    setContent (index, content) {
+      this.files[index].content = content
+    }
+  },
+  components: {
+    'editor': Editor
+  }
+}
+
+</script>
+
+<style scoped>
+
+.wrap {
+  text-align: center;
+}
+
+.description {
+  margin: auto;
+  width: 98%;
+}
+
+.nopad {
+  text-align: left;
+  padding: 0 0 0 0;
+}
+
+.filename {
+  background-color: #efefefef;
+  margin: auto;
+}
+
+.filename .input {
+  width: 40%;
+}
+
+</style>
