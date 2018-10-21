88fd6554d4fa783c3b7a88406ea59c75439a3579
Asck <asck.bbc@gmail.com>
2018-10-21 13:32:08 +0900

    edit menu css and add responsive style

--- Kusunoki-19/github-app/github-app/src/app/menu/menu.component.ts
+++ Kusunoki-19/github-app/github-app/src/app/menu/menu.component.ts
@@ -11,12 +11,13 @@ export class MenuComponent implements OnInit {
     }
 
     menuItems: {name : string, parent: string, url: string}[] = [
-        {name : "HOME" , parent:"", url:"/"},
-        {name : "OAuth認証" , parent:"", url:"/oauth"},
-        {name : "Gist" , parent:"", url:""},
+
+        {name : "HOME"                 , parent:""     , url:"/"},
+        {name : "OAuth認証"           , parent:""     , url:"/oauth"},
+        {name : "Gist"                 , parent:""     , url:""},
         {name : "All gist(だれでも!)", parent:"Gist", url:"/all-gist"},
-        {name : "All gist(User指定)", parent:"Gist", url:"/all-gist-user"},
-        {name : "post gist", parent:"Gist", url:"/post-gist"},
+        {name : "All gist(User指定)" , parent:"Gist", url:"/all-gist-user"},
+        {name : "post gist"           , parent:"Gist", url:"/post-gist"},
     ];
 
     ngOnInit() {
