9e260dbcc904844192a934ea8ebed12c5e599fb3
tockn <s.taku.inthebox@gmail.com>
2018-10-23 02:38:45 +0900

    gist card でuserも表示

--- tockn/view/src/components/parts/GistCard.vue
+++ tockn/view/src/components/parts/GistCard.vue
@@ -1,15 +1,28 @@
 <template>
   <router-link class="a" v-bind:to="`/gists/${gist.id}`" >
     <div class="card">
+
       <div class="title">
         <p>{{ cutDesc(gist.description) }}</p>
       </div>
+
       <div class="content">
         <p v-for="(name, index) in fileNames(gist.files)" :key="index">{{ name }}</p>
       </div>
+
       <div class="comments">
         <comments :num="gist.comments" />
       </div>
+
+      <router-link :to="`/users/${gist.owner.login}`" class="link">
+        <div class="avatar">
+          <img class="avatar" :src="gist.owner.avatar_url" alt="">
+        </div>
+        <div class="username">
+          {{ gist.owner.login }}
+        </div>
+      </router-link>
+
     </div>
   </router-link>
 </template>
@@ -55,5 +68,25 @@ export default {
 
 .comments {
   float: right;
+}
+
+.link {
+  color: black;
+  border-radius: 8px;
+  padding: 4px 8px;
+  border: solid 1px #000;
+}
+
+.avatar {
+  width: 24px;
+  height: 24px;
+  display: inline-block;
+  vertical-align: middle;
+}
+
+.username {
+  margin: 0 4px 0 4px;
+  display: inline-block;
+  vertical-align: middle;
 }
 </style>
