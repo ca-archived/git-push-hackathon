ee923ef8e6942419ddfd64fc6fd7cf48d9399a2b
kobakakzu0429 <kobakazu10@gmail.com>
2020-02-22 19:12:52 +0900

    add AddPlaylists

---
+++ kobakazu0429/src/components/AddPlaylists.tsx
@@ -0,0 +1,66 @@
+import React, { FC, useContext, useCallback, useState } from "react";
+import styled from "styled-components";
+import { BrandButton } from "@/components/Button";
+import { TextField } from "@/components/TextField";
+import RootContext from "@/contexts/RootContext";
+import { ModalContext } from "@/utils/customHooks/useModal";
+
+export const AddPlaylists: FC = () => {
+  const { youtubeStore } = useContext(RootContext);
+  const { closeModal } = useContext(ModalContext);
+  const [playlistName, setPlaylistName] = useState("");
+
+  const handleAddPlaylistItem = useCallback(() => {
+    if (playlistName) {
+      youtubeStore.playlistsApi?.insertPlaylists(playlistName);
+      setPlaylistName("");
+    }
+    closeModal();
+  }, [playlistName]);
+
+  const handleCancel = useCallback(() => {
+    setPlaylistName("");
+    closeModal();
+  }, []);
+
+  return (
+    <Wrapper>
+      <label htmlFor="insertPlaylistsUrlForm">
+        プレイリストの名前を入力してください
+      </label>
+      <TextField
+        name="insertPlaylistsUrlForm"
+        placeholder="お気に入りMusicリスト"
+        value={playlistName}
+        onChange={setPlaylistName}
+      />
+      <Controlls>
+        <BrandButton maxWidth="50%" onClick={handleAddPlaylistItem}>
+          追加する
+        </BrandButton>
+        <BrandButton maxWidth="50%" inverse onClick={handleCancel}>
+          キャンセル
+        </BrandButton>
+      </Controlls>
+    </Wrapper>
+  );
+};
+
+const Wrapper = styled.div`
+  width: 80%;
+  height: 200px;
+  padding: 10px;
+  background-color: #fff;
+`;
+
+const Controlls = styled.div`
+  display: flex;
+  flex-direction: row-reverse;
+
+  button:first-child {
+    margin-left: 5px;
+  }
+  button:last-child {
+    margin-right: 5px;
+  }
+`;
--- kobakazu0429/src/pages/TopPage.tsx
+++ kobakazu0429/src/pages/TopPage.tsx
@@ -1,35 +1,53 @@
-import React, { FC, useEffect, useContext } from "react";
+import React, { FC, useCallback, useEffect, useContext } from "react";
 import styled from "styled-components";
 import { Link } from "react-router-dom";
 import { observer } from "mobx-react-lite";
 import RootContext from "@/contexts/RootContext";
 import { MovieItem } from "@/components/MovieItem";
+import { BrandButton } from "@/components/Button";
+import { ModalContext } from "@/utils/customHooks/useModal";
+import { AddPlaylists } from "@/components/AddPlaylists";
 
 export const TopPage: FC = observer(() => {
   const { youtubeStore } = useContext(RootContext);
+  const { openModal, setContent } = useContext(ModalContext);
 
   useEffect(() => {
     youtubeStore.fetchPlaylists();
   }, []);
 
-  useEffect(() => {
-    youtubeStore.fetchPlaylistItems(youtubeStore?.playlists[1]?.id ?? "");
-  }, [youtubeStore.playlists]);
+  const insertAddPlaylists2Modal = useCallback(() => {
+    setContent(<AddPlaylists />);
+    openModal();
+  }, []);
 
   return (
-    <Wrapper>
-      {youtubeStore.playlists.map(({ id, snippet }) => (
-        <Link to={`/player?playlistId=${id}`} key={id}>
-          <MovieItem
-            title={snippet?.title ?? ""}
-            thumbnail={snippet?.thumbnails.standard}
-          />
-        </Link>
-      ))}
-    </Wrapper>
+    <>
+      <Wrapper>
+        {youtubeStore.playlists.map(({ id, snippet }) => (
+          <Link to={`/player?playlistId=${id}`} key={id}>
+            <MovieItem
+              title={snippet?.title ?? ""}
+              thumbnail={snippet?.thumbnails.standard}
+            />
+          </Link>
+        ))}
+      </Wrapper>
+      <ButtonWrapper>
+        <BrandButton inverse maxWidth="80%" onClick={insertAddPlaylists2Modal}>
+          プレイリストを追加する
+        </BrandButton>
+      </ButtonWrapper>
+    </>
   );
 });
 
 const Wrapper = styled.div`
   padding: 15px;
+`;
+
+const ButtonWrapper = styled.div`
+  button {
+    margin: 0 auto;
+  }
 `;
