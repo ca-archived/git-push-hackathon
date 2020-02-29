caf43c961a04788fe3c4972fab433640183a36f3
fujiwo <ffjlabo@gmail.com>
2020-02-29 13:05:32 +0900

    プレイリスト詳細画面へのリンクを追加

--- ffjlabo/src/components/pages/Playlist/Playlist.js
+++ ffjlabo/src/components/pages/Playlist/Playlist.js
@@ -1,14 +1,17 @@
 import React, {useEffect} from 'react';
 import {useSelector} from 'react-redux';
+import {Link} from 'react-router-dom';
 
 const Playlist = ({playlists}) => {
   return (
     <div>
       {playlists.map(({id, snippet}) => (
         <div key={id}>
-          <img src={snippet.thumbnails.default.url} />
-          {snippet.title}
-          {snippet.description}
+          <Link to={`/playlist/${id}`}>
+            <img src={snippet.thumbnails.default.url} />
+            {snippet.title}
+            {snippet.description}
+          </Link>
         </div>
       ))}
     </div>
